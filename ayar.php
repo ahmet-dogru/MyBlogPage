b70bd26faef803dcddab4cf25338c9c7862b6d7e
Ahmet Doğru <ahmeteileti@outlook.com>
2021-10-12 17:27:26 +0300

    Update ayar.php

--- ayar.php
+++ ayar.php
@@ -1,7 +1,7 @@
 <?php
 	try{
 		$vt = new PDO("mysql:host=rapicaus.com;dbname=rapicaus_dbblog;charset=utf8;",
-		"rapicaus_ahmet", "Eskanarya26");
+		"rapicaus_ahmet", "sifre");
 	}catch(PDOExeption $ahmet) {
 		echo $ahmet->getMessage();
 	}
